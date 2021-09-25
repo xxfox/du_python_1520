e92a3a8dabd5fa51f3fe57d9315c391310c9c00b
Oksana <xxfox.o@yandex.ru>
2021-09-25 15:34:22 +0300

    Hw lesson 8

---
+++ hw_8_04.py
@@ -0,0 +1,20 @@
+def val_checker(condition):
+    def _val_checker(func):
+        def wrapper(*args, **kwargs):
+            for item in args:
+                if condition(item):
+                    return func(*args, **kwargs)
+                else:
+                    raise ValueError('Wrong value, please try again')
+        return wrapper
+    return _val_checker
+
+
+@val_checker(lambda x: x > 0)
+def calc_cube(*args):
+    for item in args:
+        print(item ** 3)
+
+
+calc_cube(5)
+calc_cube(-5)
