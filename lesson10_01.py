2d2d6f8c8f55d1bd9eeeff5e2b8245815989ba8e
Oksana <xxfox.o@yandex.ru>
2021-10-24 21:17:42 +0300

    HW lesson 10

---
+++ lesson10_01.py
@@ -0,0 +1,31 @@
+matrix_01 = [[1, 2, 3], [3, 2, 1]]
+matrix_02 = [[4, 5, 6], [6, 5, 4]]
+
+
+class Matrix:
+
+    def __init__(self, matrix):
+        self.matrix = matrix
+
+    def __iter__(self):
+        str_matrix = (item for item in self.matrix)
+        while True:
+            try:
+                print(next(str_matrix))
+            except StopIteration:
+                break
+
+    def __add__(self, other):
+        if len(self.matrix) == len(other.matrix):
+            result_matrix = [[cell_1 + cell_2 for cell_1, cell_2 in zip(row_1, row_2)] for row_1, row_2 in
+                             zip(self.matrix, other.matrix)]
+            return f'Результат сложения двух матриц, {self.matrix} & {other.matrix}: {result_matrix}'
+        else:
+            return 'Нельзя суммировать матрицы разной длины'
+
+
+a = Matrix(matrix_01)
+b = Matrix([[4, 5, 6], [6, 5, 4]])
+
+a.__iter__()
+print(a + b)
